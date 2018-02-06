04843bddbfdaf4935522e544a7d81b8117c2845a
Nick Young <nickyoung@Nicks-MacBook-Air.local>
2018-02-06 11:52:44 +1100

    Made item a class to fix checked status

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nick Young on 6/2/18.
+//  Copyright © 2018 Nick Young. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done: Bool = false
+}
